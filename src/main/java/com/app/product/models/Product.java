418b59e8943512d301ec0a09511f221c2ed340c4
pedrohubner <pedronhubner@gmail.com>
2020-07-06 17:24:46 -0300

    Removendo anotação @Data, @NoArgsConstructor e adicionando final em cada atributo da classe

--- src/main/java/com/app/product/models/Product.java
+++ src/main/java/com/app/product/models/Product.java
@@ -1,23 +1,21 @@
 package com.app.product.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
 @Entity
+@AllArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private Double value;
+    private final Long id;
+    private final String name;
+    private final Double value;
 }
