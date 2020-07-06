2a2e8def89cb02f3f59829dfeadfec6781854b46
pedrohubner <pedronhubner@gmail.com>
2020-07-06 17:04:14 -0300

    Alterando modificador de acesso da classe de default para public

--- src/main/java/com/app/adress/models/Address.java
+++ src/main/java/com/app/adress/models/Address.java
@@ -1,5 +1,6 @@
-package com.app.client;
+package com.app.adress.models;
 
+import com.app.country.model.Country;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-class Address {
+public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
