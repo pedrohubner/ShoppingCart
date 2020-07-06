fae8b581b5fae3ac3163c90a97b82e438079cdef
pedrohubner <pedronhubner@gmail.com>
2020-07-06 17:04:42 -0300

    Criando classe repositório para Address

---
+++ src/main/java/com/app/adress/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.app.adress.repositories;
+
+import com.app.adress.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
