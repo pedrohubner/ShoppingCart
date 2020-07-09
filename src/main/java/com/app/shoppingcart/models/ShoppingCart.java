3d809aa223505d886c70946d881b5c713e4871c5
pedrohubner <pedronhubner@gmail.com>
2020-07-09 10:28:22 -0300

    Add anotação @Fetch(FetchMode.JOIN) ao método productsList

--- src/main/java/com/app/shoppingcart/models/ShoppingCart.java
+++ src/main/java/com/app/shoppingcart/models/ShoppingCart.java
@@ -5,6 +5,8 @@ import com.app.product.models.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -21,5 +23,6 @@ public class ShoppingCart {
     @OneToOne
     private Client client;
     @OneToMany
+    @Fetch(FetchMode.JOIN)
     private List<Product> productsList;
 }
