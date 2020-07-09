991ea5b478858a4b663e9f77313e7e260ef625bf
pedrohubner <pedronhubner@gmail.com>
2020-07-09 14:54:55 -0300

    Alterando nome de classe ShoppingCartFacade para AppFacade

--- src/main/java/com/app/product/controllers/ProductController.java
+++ src/main/java/com/app/product/controllers/ProductController.java
@@ -1,6 +1,6 @@
 package com.app.product.controllers;
 
-import com.app.ShoppingCartFacade;
+import com.app.AppFacade;
 import com.app.product.models.Product;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,25 +12,25 @@ import java.util.Optional;
 @RequestMapping(value = "/product")
 public class ProductController {
 
-    private final ShoppingCartFacade shoppingCartFacade;
+    private final AppFacade appFacade;
 
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
-        return shoppingCartFacade.createProduct(product);
+        return appFacade.createProduct(product);
     }
 
     @GetMapping(value = "/{id}")
     public Optional<Product> findProduct(@PathVariable Long id) {
-        return shoppingCartFacade.findProductById(id);
+        return appFacade.findProductById(id);
     }
 
     @DeleteMapping
     public void deleteAllProduct() {
-        shoppingCartFacade.deleteAllProducts();
+        appFacade.deleteAllProducts();
     }
 
     @DeleteMapping(value = "/{id}")
     public void deleteProduct(@PathVariable Long id) {
-        shoppingCartFacade.deleteProductById(id);
+        appFacade.deleteProductById(id);
     }
 }
