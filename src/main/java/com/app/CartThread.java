87d85e8451bbad2be7f5c54f062f6504e7ae9250
pedrohubner <pedronhubner@gmail.com>
2020-07-09 10:27:43 -0300

    Criando atributos Long cartId e productId, dependências de semaphore, shoppingCart e shoppingCartProductService e implementando Runnable e seu método run

---
+++ src/main/java/com/app/CartThread.java
@@ -0,0 +1,48 @@
+package com.app;
+
+import com.app.shoppingcart.models.ShoppingCart;
+import com.app.shoppingcartproducts.services.ShoppingCartProductsService;
+
+import java.util.concurrent.Semaphore;
+
+public class CartThread implements Runnable{
+
+    private final Long cartId;
+    private final Long productId;
+    private ShoppingCart shoppingCart;
+    private final Semaphore semaphore;
+    private final ShoppingCartProductsService shoppingCartProductsService;
+
+    public CartThread(Semaphore semaphore, ShoppingCartProductsService shoppingCartProductsService, Long productId, Long cartId) {
+        this.semaphore = semaphore;
+        this.shoppingCartProductsService = shoppingCartProductsService;
+        this.productId = productId;
+        this.cartId = cartId;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " foi inicializada");
+
+            semaphore.acquire();
+
+            System.out.println(Thread.currentThread().getName() + " pegou a permissão");
+
+            Thread.sleep(10000);
+
+            shoppingCart = shoppingCartProductsService.addProductToShoppingCart(cartId, productId);
+            System.out.println(Thread.currentThread().getName() + " foi executada");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+            System.out.println(Thread.currentThread().getName() + " liberou permissão");
+        }
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+}
