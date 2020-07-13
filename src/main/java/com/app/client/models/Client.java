dd232c6d5e7001abe23a69a76ef9fe436324e07d
pedrohubner <pedronhubner@gmail.com>
2020-07-13 10:27:37 -0300

    Implementando Cloneable, método clone() e anotação @ToString.Exclude ao método Long id

--- src/main/java/com/app/client/models/Client.java
+++ src/main/java/com/app/client/models/Client.java
@@ -1,23 +1,28 @@
 package com.app.client.models;
 
 import com.app.address.models.Address;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Client {
+public class Client implements Cloneable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Exclude
     private Long id;
     private String name;
     private String document;
     @OneToOne
     private Address address;
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
