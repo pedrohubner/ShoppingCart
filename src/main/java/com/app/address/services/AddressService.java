c2d53be8ca625c24562bdebf067942e2e8fb9ef8
pedrohubner <pedronhubner@gmail.com>
2020-07-13 09:08:19 -0300

    Criando métodos createAddress e deleteAddressById

--- src/main/java/com/app/address/services/AddressService.java
+++ src/main/java/com/app/address/services/AddressService.java
@@ -1,5 +1,6 @@
 package com.app.address.services;
 
+import com.app.address.models.Address;
 import com.app.address.repositories.AddressRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AddressService {
 
-    private AddressRepository addressRepository;
+    private final AddressRepository addressRepository;
+
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public void deleteAddressById(Long id) {
+        addressRepository.deleteById(id);
+    }
 }
