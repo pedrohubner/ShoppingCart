67ba54c72d1bfe0d395493c95afe0845140f6fab
pedrohubner <pedronhubner@gmail.com>
2020-07-13 09:07:18 -0300

    Criando métodos createAddress, deleteAddressById e alterando nome da classe facade

--- src/main/java/com/app/address/controllers/AddressController.java
+++ src/main/java/com/app/address/controllers/AddressController.java
@@ -1,14 +1,24 @@
 package com.app.address.controllers;
 
-import com.app.address.services.AddressService;
+import com.app.AppFacade;
+import com.app.address.models.Address;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/address")
 @AllArgsConstructor
 public class AddressController {
 
-    private AddressService addressService;
+    private final AppFacade appFacade;
+
+    @PostMapping
+    public Address createAddress(@RequestBody Address address) {
+        return appFacade.createAddress(address);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteAddressById(@PathVariable Long id) {
+        appFacade.deleteAddress(id);
+    }
 }
