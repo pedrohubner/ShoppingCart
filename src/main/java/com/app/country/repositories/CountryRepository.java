d9f98d12bdf800da1b59a99b80b311d8dd3c24cf
pedrohubner <pedronhubner@gmail.com>
2020-07-13 09:11:01 -0300

    Alterando elemento de JpaRepository de Client para Country

--- src/main/java/com/app/country/repositories/CountryRepository.java
+++ src/main/java/com/app/country/repositories/CountryRepository.java
@@ -1,9 +1,9 @@
 package com.app.country.repositories;
 
-import com.app.client.models.Client;
+import com.app.country.models.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryRepository extends JpaRepository<Client, Long> {
+public interface CountryRepository extends JpaRepository<Country, Long> {
 }
