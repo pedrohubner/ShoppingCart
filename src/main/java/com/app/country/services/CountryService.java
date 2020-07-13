9cafe888ba8fc8c80c6ad831328afa3498bac04b
pedrohubner <pedronhubner@gmail.com>
2020-07-13 09:11:23 -0300

    Criando métodos createCountry e deleteCountryById

--- src/main/java/com/app/country/services/CountryService.java
+++ src/main/java/com/app/country/services/CountryService.java
@@ -1,9 +1,21 @@
 package com.app.country.services;
 
+import com.app.country.models.Country;
+import com.app.country.repositories.CountryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public Country createCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    public void deleteCountryById(Long id) {
+        countryRepository.deleteById(id);
+    }
 }
