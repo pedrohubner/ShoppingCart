c4dd430646250167a5b101ba3e674898c18264fc
pedrohubner <pedronhubner@gmail.com>
2020-07-14 14:22:09 -0300

    Edentação de código

--- src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
+++ src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "/cart")
-public class ShoppingCartController  {
+public class ShoppingCartController {
 
     private final AppFacade appFacade;
 
