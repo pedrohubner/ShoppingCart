7dba4b5637c854b2697ba009142488b05ce30903
pedrohubner <pedronhubner@gmail.com>
2020-07-15 08:17:48 -0300

    Implementando annotation @ResponseStatus com parâmetro HTTP.NOT_FOUND

--- src/main/java/com/app/product/exceptionhandler/ProductException.java
+++ src/main/java/com/app/product/exceptionhandler/ProductException.java
@@ -1,5 +1,9 @@
 package com.app.product.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductException extends RuntimeException {
 
     public ProductException(String msg) {
