af03bff4c37b305f166b30d88308ca812bbf1d55
pedrohubner <pedronhubner@gmail.com>
2020-07-16 17:47:24 -0300

    Criando método getDTOList

--- src/main/java/com/app/product/services/ProductService.java
+++ src/main/java/com/app/product/services/ProductService.java
@@ -1,10 +1,15 @@
 package com.app.product.services;
 
+import com.app.DTOFactory;
 import com.app.product.exceptionhandler.ProductException;
 import com.app.product.models.Product;
+import com.app.product.models.ProductDTO;
 import com.app.product.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -51,5 +56,12 @@ public class ProductService {
 
     public void deleteAllProducts() {
         productRepository.deleteAllInBatch();
+    }
+
+    public List<ProductDTO> getDTOList() {
+        return productRepository.findAll()
+                .stream()
+                .map(DTOFactory::mapToProductDTO)
+                .collect(Collectors.toList());
     }
 }
