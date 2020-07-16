95c4fe50b0f6b80fd6a8bc61f938ee0b8f18b7ca
pedrohubner <pedronhubner@gmail.com>
2020-07-16 17:47:41 -0300

    Criando método getDTOCartList

--- src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
+++ src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
@@ -2,8 +2,11 @@ package com.app.shoppingcart.controllers;
 
 import com.app.AppFacade;
 import com.app.shoppingcart.models.ShoppingCart;
+import com.app.shoppingcart.models.ShoppingCartDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -21,6 +24,11 @@ public class ShoppingCartController {
     public ShoppingCart findCart(@PathVariable Long id) {
         return appFacade.findShoppingCartById(id);
     }
+//
+//    @GetMapping(value = "/tst")
+//    public List<ShoppingCartDTO> getDTOCartList() {
+//        return appFacade.getDTOCartList();
+//    }
 
     @DeleteMapping(value = "/{id}")
     public void deleteCart(@PathVariable Long id) {
