52baf52f9cacae3ec0a6ce19ec44bb0c1a7e3892
pedrohubner <pedronhubner@gmail.com>
2020-07-19 17:32:16 -0300

    Mudando retorno do método addProductToShoppingCart de ShoppingCart para CartThread

--- src/main/java/com/app/shoppingcartproducts/controllers/ShoppingCartProductsController.java
+++ src/main/java/com/app/shoppingcartproducts/controllers/ShoppingCartProductsController.java
@@ -1,6 +1,7 @@
 package com.app.shoppingcartproducts.controllers;
 
 import com.app.AppFacade;
+import com.app.CartThread;
 import com.app.shoppingcart.models.ShoppingCart;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ public class ShoppingCartProductsController {
     private final AppFacade appFacade;
 
     @PostMapping(value = "/{cartId}/products/{productId}")
-    public ShoppingCart addProductToShoppingCart(@PathVariable Long cartId, @PathVariable Long productId) {
+    public CartThread addProductToShoppingCart(@PathVariable Long cartId, @PathVariable Long productId) {
         return appFacade.addProductToShoppingCart(cartId, productId);
     }
 
