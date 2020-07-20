d24b62c83c9624edf70677a9b6441c7ba69ff9f9
pedrohubner <pedronhubner@gmail.com>
2020-07-20 08:54:52 -0300

    Deletando classes address, client e country

--- src/main/java/com/app/address/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.address.repositories;
-
-import com.app.address.models.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- src/main/java/com/app/client/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.client.repositories;
-
-import com.app.client.models.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
