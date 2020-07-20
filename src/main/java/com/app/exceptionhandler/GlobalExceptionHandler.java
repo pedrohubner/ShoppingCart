8fe8cf299f53360f54735975e0f8d6b7facf6855
pedrohubner <pedronhubner@gmail.com>
2020-07-20 11:17:41 -0300

    Deletando método productNotFound, passando ApiException como parâmetro do método cartNotFound, e alterando corpo do método

--- src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.app.exceptionhandler;
 
-import com.app.product.exceptionhandler.ProductException;
-import com.app.shoppingcart.exceptionhandler.ShoppingCartException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,28 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiException> genericException() {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiException.builder()
+    public ResponseEntity<ExceptionBody> genericException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ExceptionBody.builder()
                 .message("Ocorreu um erro inesperado")
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .build());
     }
 
-    @ExceptionHandler(ShoppingCartException.class)
-    public ResponseEntity<ApiException> cartNotFound(ShoppingCartException shoppingCart) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiException.builder()
-                .message(shoppingCart.getMessage())
-                .status(HttpStatus.NOT_FOUND)
-                .suggestion("Digite o id de um carrinho existente")
-                .build());
-    }
-
-    @ExceptionHandler(ProductException.class)
-    public ResponseEntity<ApiException> productNotFound(ProductException product) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiException.builder()
-                .message(product.getMessage())
-                .status(HttpStatus.NOT_FOUND)
-                .suggestion("Digite o id de um produto existente")
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ExceptionBody> cartNotFound(ApiException apiException) {
+        return ResponseEntity.status(apiException.getStatus())
+                .body(ExceptionBody.builder()
+                .message(apiException.getMessage())
+                .status(apiException.getStatus())
+                .suggestion(apiException.getSuggestion())
                 .build());
     }
 }
