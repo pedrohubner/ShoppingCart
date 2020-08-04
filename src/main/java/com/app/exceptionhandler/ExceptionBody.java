d0789936daffb9250b319b5b62fde3a69b0b6dbe
pedrohubner <pedronhubner@gmail.com>
2020-08-04 14:34:04 -0300

    Removendo modificador de acesso public da classe ExceptionBody

--- src/main/java/com/app/exceptionhandler/ExceptionBody.java
+++ src/main/java/com/app/exceptionhandler/ExceptionBody.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ExceptionBody {
+class ExceptionBody {
 
     private String message;
     private HttpStatus status;
