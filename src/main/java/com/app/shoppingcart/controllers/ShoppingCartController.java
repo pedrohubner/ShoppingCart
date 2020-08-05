6a964f2501d3bc6c26948c78ec3f072f9905ff45
pedrohubner <pedronhubner@gmail.com>
2020-08-04 22:46:20 -0300

    Alterando mensagens de @ApiOperation

--- src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
+++ src/main/java/com/app/shoppingcart/controllers/ShoppingCartController.java
@@ -19,25 +19,26 @@ public class ShoppingCartController {
     private final AppFacade appFacade;
 
     @PostMapping
-    @ApiOperation(value = "Cria um novo cart")
+    @ApiOperation(value = "Cria um novo objeto ShoppingCart.")
     public ShoppingCart createCart(@RequestBody ShoppingCart shoppingCart) {
         return appFacade.createShoppingCart(shoppingCart);
     }
 
     @GetMapping(value = "/{id}")
-    @ApiOperation(value = "Verifica se o cart existe e retorna ele")
+    @ApiOperation(value = "Verifica se o objeto ShoppingCart existe pelo seu id e retorna ele, " +
+            "caso não exista, exibe mensagem de erro.")
     public ShoppingCart findCart(@PathVariable Long id) {
         return appFacade.findShoppingCartById(id);
     }
 
     @GetMapping(value = "/tst")
-    @ApiOperation(value = "Retorna cart sem o id")
+    @ApiOperation(value = "Retorna um objeto JSON de ShoppingCart em que o campo id não aparece.")
     public List<ShoppingCartDTO> getDTOCartList() {
         return appFacade.getDTOCartList();
     }
 
     @DeleteMapping(value = "/{id}")
-    @ApiOperation(value = "Deleta cart pelo seu id")
+    @ApiOperation(value = "Deleta um objeto ShoppingCart pelo seu id")
     public void deleteCart(@PathVariable Long id) {
         appFacade.deleteShoppingCart(id);
     }
