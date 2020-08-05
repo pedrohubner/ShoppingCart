05118e6460eea190e9d05e4de97e2986ef96cc6b
pedrohubner <pedronhubner@gmail.com>
2020-08-05 14:51:23 -0300

    Removendo @Mock de ShoppingCart e Product, e implementando corpo dos métodos addProductToShoppingCart_ifIdNotNull e deleteProductFromShoppingCart_ifIdNotNull.

--- src/test/java/com/app/shoppingcartproducts/controllers/ShoppingCartProductsControllerTest.java
+++ src/test/java/com/app/shoppingcartproducts/controllers/ShoppingCartProductsControllerTest.java
@@ -1,30 +1,32 @@
 package com.app.shoppingcartproducts.controllers;
 
 import com.app.facade.AppFacade;
-import com.app.product.models.Product;
-import com.app.shoppingcart.models.ShoppingCart;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ShoppingCartProductsControllerTest {
-
-    @Mock
-    Product product;
 
     @Mock
     AppFacade appFacade;
 
     @Mock
-    ShoppingCart shoppingCart;
+    ShoppingCartProductsController shoppingCartProductsController;
 
     @Test
     public void addProductToShoppingCart_ifIdNotNull() {
+        shoppingCartProductsController.addProductToShoppingCart(1L, 1L);
+        verify(appFacade, times(1)).addProductToShoppingCart(1L, 1L);
     }
 
     @Test
     public void deleteProductFromShoppingCart_ifIdNotNull() {
+        shoppingCartProductsController.removeProductFromShoppingCart(1L, 1L);
+        verify(appFacade, times(1)).removeProductFromShoppingCart(1L, 1L);
     }
 }
