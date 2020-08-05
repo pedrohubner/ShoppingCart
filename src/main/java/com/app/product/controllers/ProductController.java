020fdcb889f9d13867eff62b44dc7818a0e4d38f
pedrohubner <pedronhubner@gmail.com>
2020-08-04 22:46:13 -0300

    Alterando mensagens de @ApiOperation

--- src/main/java/com/app/product/controllers/ProductController.java
+++ src/main/java/com/app/product/controllers/ProductController.java
@@ -19,31 +19,32 @@ public class ProductController {
     private final AppFacade appFacade;
 
     @PostMapping
-    @ApiOperation(value = "Cria um novo product")
+    @ApiOperation(value = "Cria um novo objeto de Product.")
     public Product createProduct(@RequestBody Product product) {
         return appFacade.createProduct(product);
     }
 
     @GetMapping(value = "/{id}")
-    @ApiOperation(value = "Verifica se o product existe e retorna ele")
+    @ApiOperation(value = "Verifica se o objeto Product existe  pelo id e retorna ele," +
+            " caso não exista, imprime mensagem de erro.")
     public Product findProduct(@PathVariable Long id) {
         return appFacade.findProductById(id);
     }
 
     @GetMapping(value = "/tst")
-    @ApiOperation(value = "Retorna um product sem o id")
+    @ApiOperation(value = "Retorna um objeto JSON de Product em que o campo id não aparece.")
     public List<ProductDTO> getDTOProductList() {
         return appFacade.getDTOProductList();
     }
 
     @DeleteMapping
-    @ApiOperation(value = "Deleta todos os products")
+    @ApiOperation(value = "Deleta todos os objetos Product.")
     public void deleteAllProduct() {
         appFacade.deleteAllProducts();
     }
 
     @DeleteMapping(value = "/{id}")
-    @ApiOperation(value = "Deleta um product pelo seu id")
+    @ApiOperation(value = "Deleta um objeto Product pelo seu id.")
     public void deleteProduct(@PathVariable Long id) {
         appFacade.deleteProductById(id);
     }
