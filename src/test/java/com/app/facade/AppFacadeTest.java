b9ba70522abe99618348b8150526877a6dabfeb8
pedrohubner <pedronhubner@gmail.com>
2020-08-04 22:58:41 -0300

    Implementando métodos para teste

--- src/test/java/com/app/facade/AppFacadeTest.java
+++ src/test/java/com/app/facade/AppFacadeTest.java
@@ -1,14 +1,21 @@
 package com.app.facade;
 
+import com.app.product.models.Product;
+import com.app.product.models.ProductDTO;
 import com.app.product.services.ProductDTOService;
 import com.app.product.services.ProductService;
+import com.app.shoppingcart.models.ShoppingCart;
+import com.app.shoppingcart.models.ShoppingCartDTO;
 import com.app.shoppingcart.repositories.ShoppingCartRepository;
 import com.app.shoppingcart.services.ShoppingCartDTOService;
 import com.app.shoppingcart.services.ShoppingCartService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AppFacadeTest {
@@ -30,4 +37,48 @@ public class AppFacadeTest {
 
     @InjectMocks
     AppFacade appFacade;
+
+    @Test
+    public void createProduct() {
+    }
+
+    @Test
+    public void findProductById() {
+    }
+
+    @Test
+    public void getDTOProductList() {
+    }
+
+    @Test
+    public void deleteProductById() {
+    }
+
+    @Test
+    public void deleteAllProducts() {
+    }
+
+    @Test
+    public void createShoppingCart() {
+    }
+
+    @Test
+    public void findShoppingCartById() {
+    }
+
+    @Test
+    public void getDTOCartList() {
+    }
+
+    @Test
+    public void deleteShoppingCart() {
+    }
+
+    @Test
+    public void addProductToShoppingCart() {
+    }
+
+    @Test
+    public void removeProductFromShoppingCart() {
+    }
 }
