b4e139b76b4b0a7356f2cce23b51f6a74cff3668
pedrohubner <pedronhubner@gmail.com>
2020-08-24 17:42:16 -0300

    Criando testes should_Return_Product_When_Repository_Saves_Product_In_H2(), should_Return_Product_When_Repository_Finds_Product_By_Id() e should_Delete_Product_When_Repository_Finds_Product_By_Id()

---
+++ src/test/java/com/app/ProductIntegrationTest.java
@@ -0,0 +1,74 @@
+package com.app;
+
+import com.app.product.models.Product;
+import com.app.product.repositories.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ProductIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    public void should_Return_Product_When_Repository_Saves_Product_In_H2() throws Exception {
+        Product product = new Product(1L, "Dorflex", 15., 12);
+
+        mockMvc.perform(post("/products")
+                .content(objectMapper.writeValueAsString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Product product1 = productRepository.findById(1L).get();
+
+        Assert.assertEquals(product1, product);
+    }
+
+    @Test
+    public void should_Return_Product_When_Repository_Finds_Product_By_Id() throws Exception {
+        Product product = new Product(1L, "Dorflex", 15., 12);
+
+        productRepository.save(product);
+
+        mockMvc.perform(get("/products/{id}", 1L)
+                .content(objectMapper.writeValueAsString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Product product1 = productRepository.findById(1L).get();
+
+        Assert.assertEquals(product1, product);
+    }
+
+    @Test
+    public void should_Delete_Product_When_Repository_Finds_Product_By_Id() throws Exception {
+        Product product = new Product(2L, "Azitromicina", 24., 21);
+
+        productRepository.save(product);
+
+        mockMvc.perform(delete("/products/{id}", 2L)
+                .content(objectMapper.writeValueAsString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
