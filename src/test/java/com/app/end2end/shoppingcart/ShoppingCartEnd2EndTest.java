ed3ecc88d2c2aa17b6850fce4eefae4da9bdb3b6
pedrohubner <pedronhubner@gmail.com>
2020-09-02 08:31:10 -0300

    Deletando teste should_Return_Shopping_Cart_When_Repository_Finds_Shopping_Cart_By_Id() e imports não utilizados

--- src/test/java/com/app/end2end/shoppingcart/ShoppingCartEnd2EndTest.java
+++ src/test/java/com/app/end2end/shoppingcart/ShoppingCartEnd2EndTest.java
@@ -1,23 +1,17 @@
 package com.app.end2end.shoppingcart;
 
-import com.app.product.models.Product;
 import com.app.shoppingcart.models.ShoppingCart;
 import com.app.shoppingcart.repositories.ShoppingCartRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -33,46 +27,6 @@ public class ShoppingCartEnd2EndTest {
 
     @Autowired
     ShoppingCartRepository shoppingCartRepository;
-
-    @Test
-    public void should_Return_Shopping_CartWhen_Repository_Saves_Shopping_Cart_In_H2() throws Exception{
-        List<Product> productList = new ArrayList<>();
-
-        ShoppingCart shoppingCart = ShoppingCart.builder()
-                .id(1L)
-                .productsList(productList)
-                .build();
-
-        mockMvc.perform(post("/carts")
-                .content(objectMapper.writeValueAsString(shoppingCart))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        ShoppingCart shoppingCart1 = shoppingCartRepository.findById(1L).get();
-
-        Assert.assertEquals(shoppingCart1.getProductsList(), shoppingCart.getProductsList());
-    }
-
-    @Test
-    public void should_Return_Shopping_Cart_When_Repository_Finds_Shopping_Cart_By_Id() throws Exception {
-        List<Product> productList = new ArrayList<>();
-
-        ShoppingCart shoppingCart = ShoppingCart.builder()
-                .id(1L)
-                .productsList(productList)
-                .build();
-
-        shoppingCartRepository.save(shoppingCart);
-
-        mockMvc.perform(get("/carts/{id}", 1L)
-                .content(objectMapper.writeValueAsString(shoppingCart))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        ShoppingCart shoppingCart1 = shoppingCartRepository.findById(1L).get();
-
-        Assert.assertEquals(shoppingCart1, shoppingCart);
-    }
 
     @Test
     public void should_Delete_Shopping_Cart_When_Repository_Finds_Cart_By_Id() throws Exception {
