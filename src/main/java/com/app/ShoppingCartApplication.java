76c5044cf175320e20811d584a98145240b625ed
pedrohubner <pedronhubner@gmail.com>
2020-09-14 15:51:35 -0300

    Estendendo a classe SpringBootServletInitializer

--- src/main/java/com/app/ShoppingCartApplication.java
+++ src/main/java/com/app/ShoppingCartApplication.java
@@ -2,9 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShoppingCartApplication {
+public class ShoppingCartApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
