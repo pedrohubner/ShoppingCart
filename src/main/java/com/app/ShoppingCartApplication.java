d607e4924008647bed4568f4890eccd4fb1eede0
pedrohubner <pedronhubner@gmail.com>
2020-09-16 17:21:34 -0300

    Removendo extensão da classe SpringBootServletInitializer

--- src/main/java/com/app/ShoppingCartApplication.java
+++ src/main/java/com/app/ShoppingCartApplication.java
@@ -2,10 +2,9 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShoppingCartApplication extends SpringBootServletInitializer {
+public class ShoppingCartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
