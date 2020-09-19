80a3df1cf9bd5e40fe9d5a5840d6e546abba91be
pedrohubner <pedronhubner@gmail.com>
2020-09-18 21:25:12 -0300

    Add anotação @EnableEurekaClient

--- src/main/java/com/app/ShoppingCartApplication.java
+++ src/main/java/com/app/ShoppingCartApplication.java
@@ -2,12 +2,22 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@RestController
 @SpringBootApplication
-public class ShoppingCartApplication {
+public class ShoppingCartApplication extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
-
 }
