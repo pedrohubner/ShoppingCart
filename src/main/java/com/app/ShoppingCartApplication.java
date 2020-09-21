3379495e8c4b81dd9f25b51ed293c605da93aff4
pedrohubner <pedronhubner@gmail.com>
2020-09-21 11:36:47 -0300

    Removendo anotações @RestController e @EnableEurekaClient

--- src/main/java/com/app/ShoppingCartApplication.java
+++ src/main/java/com/app/ShoppingCartApplication.java
@@ -2,20 +2,9 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
-@RestController
 @SpringBootApplication
-public class ShoppingCartApplication extends SpringBootServletInitializer {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World";
-    }
+public class ShoppingCartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
