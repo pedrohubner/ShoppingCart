ac7aa177d823c72965ec166f6051fc63e0ad7931
pedrohubner <pedronhubner@gmail.com>
2020-09-30 15:14:06 -0300

    Add anotação @EnableEurekaClient

--- src/main/java/com/app/ShoppingCartApplication.java
+++ src/main/java/com/app/ShoppingCartApplication.java
@@ -2,9 +2,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class ShoppingCartApplication {
+public class ShoppingCartApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
